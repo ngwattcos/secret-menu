5de7756336a7a06d55aff6ab01437244d50586ed
Scott Wang <curiosity142000@gmail.com>
2019-10-09 17:57:17 -0400

    initial commit

---
+++ Secret Menu/Secret Menu/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Secret Menu
+//
+//  Created by Scott Wang on 10/9/19.
+//  Copyright © 2019 Scott Wang. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
